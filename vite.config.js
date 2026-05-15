4ad7a2f94a71ff8bc73dd9381379d71e513d64ce
vadaiat4-pixel <vadaiat4@gmail.com>
2026-05-16 00:03:20 +0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-export function getHashRoute() {
-  const value = window.location.hash.replace('#/', '').replace('#', '').split('?')[0];
-  return value || 'matchmaking';
-}
-
-export function navigate(path) {
-  window.location.hash = `#/${path}`;
-}
